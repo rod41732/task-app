1c2489a801f618a402ee55978ace2810088660d2
Road <rod8711@gmail.com>
2025-09-05 01:14:36 +0700

    add list divider

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -1,5 +1,6 @@
 "use  client";
 import type { Task } from "@/backend/task-service";
+import React from "react";
 import { TaskItem } from "./TaskItem";
 
 export function TaskList({
@@ -11,8 +12,15 @@ export function TaskList({
 }) {
   return (
     <div className="px-4 py-2  rounded-md bg-gray-900">
-      {tasks.map((it) => {
-        return <TaskItem key={it.id} task={it} reloadTask={reloadTask} />;
+      {tasks.map((it, idx) => {
+        return (
+          <React.Fragment key={it.id}>
+            <TaskItem key={it.id} task={it} reloadTask={reloadTask} />
+            {idx != tasks.length - 1 && (
+              <div className="w-full h-px bg-gray-800"></div>
+            )}
+          </React.Fragment>
+        );
       })}
       {tasks.length == 0 && (
         <div className="text-center text-gray-600">No tasks</div>
